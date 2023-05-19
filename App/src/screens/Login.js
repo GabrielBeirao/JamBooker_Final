322ee4414fd5e79c852a7287852df2dd93527274
Marcelo Boaria <marceloboaria@gmail.com>
2023-05-18 21:57:13 -0300

    aula do dia 18-05

--- App/src/screens/Login.js
+++ App/src/screens/Login.js
@@ -1,39 +1,37 @@
-import { StyleSheet, TouchableOpacity, View, Image, useWindowDimensions, Text } from "react-native";
-import React, { useState } from 'react';
-import Logo from '../assets/images/logo.png';
+import { StyleSheet, TouchableOpacity, View, Image, useWindowDimensions, Text, TextInput } from "react-native";
+import React, { useState, useContext } from 'react';
+import Logo from '../../assets/images/Logo.png';
 import CustomInput from "../components/CustomInput";
 import CustomButton from "../components/CustomButton";
-import api from "../api";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import api from '../api';
+import { Context } from '../context/dataContext';
 
 const Login = ({ navigation }) => {
-
+    const { dispatch } = useContext(Context);
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const { height } = useWindowDimensions();
 
     const onLoginPressed = async () => {
-
-
         try {
-
-            const data = await api.post('/login', {
+            const authData = await api.post('/login', {
                 email: email,
                 password: password
-            });
-            if (data.status === 200) {
-                localStorage.setItem('token', data.data.token)
-                alert(data.data.message)
-                navigation.navigate('Home')
+            })
+            if(authData.status === 200){
+                await AsyncStorage.setItem('token', authData.data.token)
+                dispatch({type:'logIn', payload: true})
             } else {
                 alert('Email ou Senha Inválidos')
                 setPassword('')
             }
-        } catch (err) {
-            alert("Erro Inesperado!");
-            console.log(err)
+        } catch (error) {
+            alert('Email ou Senha Inválidos')
+            setPassword('')
         }
-
     }
+
+    const { height } = useWindowDimensions();
 
     return (
         <View style={styles.view}>
@@ -42,18 +40,22 @@ const Login = ({ navigation }) => {
                 style={[styles.logo, { height: height * 0.3 }]}
                 resizeMode="contain"
             />
+
             <CustomInput
                 placeholder="Email"
                 value={email}
                 setValue={setEmail}
             />
+
             <CustomInput
                 placeholder="Password"
                 value={password}
                 setValue={setPassword}
                 secureTextEntry={true}
             />
+
             <CustomButton text="Login" onPress={onLoginPressed} />
+
             <TouchableOpacity
                 onPress={() => navigation.navigate("RegisterUser")}
             >
@@ -64,28 +66,25 @@ const Login = ({ navigation }) => {
                     </Text>
                 </Text>
             </TouchableOpacity>
+
         </View>
     )
 };
 
 const styles = StyleSheet.create({
-
     view: {
         alignItems: 'center',
         padding: 20,
-        marginTop: 80,
     },
     logo: {
         width: '70%',
         maxWidth: 300,
         maxHeight: 200,
     },
-
     createAccountText: {
         fontWeight: "bold",
         color: "#6200ee",
     },
-
 });
 
 export default Login;
